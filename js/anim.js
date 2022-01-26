dbd5da265e4b534b9f5e0e8de160ccdc744d4a24
jnrhey <92171893+jnrhey@users.noreply.github.com>
2022-01-26 17:51:30 +0800

    Added initials files
    Added index.html. about.html. features.html. macos.html. windows.html.

---
+++ js/anim.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.anim', {
+    strings: [
+        'Mark Angelo Erna',
+        'Jan Rhey Paaño',
+        'Yoshiya Ito',
+        'Fhill Lewis',
+        'Lyla Norella'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
